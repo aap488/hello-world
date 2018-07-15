9ca4e0be5afed0ee229e7ff0f8d3804f2b88f042
aap488 <41246641+aap488@users.noreply.github.com>
2018-07-14 23:26:22 -0500

    Add files via upload

---
+++ python_notes.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+""" Storing variables with .sort function will return NoneType. Ex.: 
+    sorted_list = my_list.sort() will return None because it changes variable
+    in place. To store in new variable you would do my_list.sort() then
+    sorted_list = my_list.sort()
